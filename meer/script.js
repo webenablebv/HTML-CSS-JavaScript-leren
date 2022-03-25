7036fa8e91a45fad819ab292c7e0bd86be08909f
thomas-webenable <thomas@webenable.nl>
2022-03-25 14:36:29 +0100

    Add files

---
+++ meer/script.js
@@ -0,0 +1,3 @@
+console.log('test 2');
+
+setTimeout(() => alert('Hoi! Dit is een ANDERE "alert box". Weet jij waar de regels code voor dit berichtje staan?'), 3000);
---
+++ welkom/script.js
@@ -0,0 +1,3 @@
+console.log('test');
+
+setTimeout(() => alert('Hoi! Dit is een "alert box". Weet jij waar de regels code voor dit berichtje staan?'), 3000);
